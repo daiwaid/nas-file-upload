1fce49aad010daf79945b266e1c4ad045271d082
David <davidspacewang@gmail.com>
2023-10-21 22:06:44 -0700

    updated server files

--- server/remove.php
+++ server/remove.php
@@ -16,11 +16,6 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
   }
 
   $base_dir = '/var/share';
-  $trash_dir = '/var/share/lan-drive/.recycle/server/';
-  if (!file_exists($trash_dir)) {
-    mkdir($trash_dir, 0775, true);
-  }
-
   // read data from post request
   $data = json_decode(file_get_contents('php://input'), true);
   $tbl_name = $data['table'];
@@ -33,8 +28,8 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
   $result = $stmt->get_result();
   $row = $result->fetch_assoc();
 
-  // save the removed images
-  rename($base_dir . $row['path'], $trash_dir . $row['name']);
+  // delete images and remove from table
+  unlink($base_dir . $row['path']);
   unlink($base_dir . $row['thumb']);
 
   $stmt = $conn->prepare("DELETE FROM $tbl_name WHERE name=?;");
